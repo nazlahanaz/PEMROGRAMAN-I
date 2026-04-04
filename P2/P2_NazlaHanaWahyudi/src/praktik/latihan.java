c46e86b0cb390ed5252655e4ab45cdce7453ba59
Nazla Hana Wahyudi <nazlahana11@gmail.com>
2026-04-04 13:51:44 +0700

    update project

--- P2/P2_NazlaHanaWahyudi/src/praktik/latihan.java
+++ P2/P2_NazlaHanaWahyudi/src/praktik/latihan.java
@@ -1,4 +1,21 @@
 package praktik;
+import java.util.Scanner;
 
 public class latihan {
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+        String nama;
+        int usia;
+
+        System.out.print("masukkan nama anda: ");
+        nama = input.nextLine();
+        System.out.print("berapa usianya: ");
+        usia = input.nextInt();
+
+        System.out.println("===============================");
+        System.out.println("HALLO " + nama);
+        System.out.println("umur anda " + usia + " yaa");
+
+    }
 }
